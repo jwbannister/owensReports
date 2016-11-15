29d1e5317d6f80704bad127c68a1b0e9670e3f9c
John Bannister <jbannister@airsci.com>
2016-11-15 14:43:54 -0800

    plot background with pleiades. About to convert to ggmap picture.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -45,16 +45,12 @@ plot_csc_site <- function(background, sand_df, area_txt,
 #' 
 #' @import dplyr
 #' @import ggplot2
-#' @param areas String.
-#' @param polys_df Data frame. Points defining polygons are DCM areas. 
-#' @param lables_df Data frame. Area labels and position coordinates.
-#' @param external_points Data frame. *x* and *y* coordinates of points external 
-#' to polygons to be included in plot range. 
-plot_dca_background <- function(areas, polys_df, labels_df, 
+#' @param polys_df 
+#' @param labels_df 
+#' @param external_points 
+plot_dca_background <- function(polys_df, labels_df, 
                                 external_points=NULL){
-  polys <- polys_df %>% filter(area %in% areas)
-  labels <- labels_df %>% filter(area %in% areas)
-  plot.range <- get_plot_range(polys, external_points)
+  plot.range <- owensMaps::get_plot_range(polys_df, external_points)
   map <- raster::stack("~/dropbox/owens/gis/pleiades/pleiades_20160824.tif")
   ext <- sp::SpatialPointsDataFrame(coords=cbind(x=plot.range$x, y=plot.range$y), 
                                 data=data.frame(id=1:2), 
@@ -66,8 +62,8 @@ plot_dca_background <- function(areas, polys_df, labels_df,
                        b=map_df[ , 5])
   p1 <- ggplot(data=map_df) + coord_fixed() + theme_bw() +
   geom_tile(aes(x=x, y=y, fill=rgb(r,g,b, maxColorValue = 255)), alpha=0.75) + 
-  geom_path(data=polys, mapping=aes(x=x, y=y, group=objectid), color="black") +
-  geom_text(data=labels, aes(x=x, y=y, label=area), color="black") +
+  geom_path(data=polys_df, mapping=aes(x=x, y=y, group=id), color="black") +
+  geom_text(data=labels_df, aes(x=x, y=y, label=id), color="black") +
   scale_fill_identity() + 
   scale_x_continuous(breaks=range(map_df$x)*c(1.01, 0.99), 
                      labels=range(map_df$x), expand = c(0,0)) +
@@ -77,28 +73,39 @@ plot_dca_background <- function(areas, polys_df, labels_df,
 p1
 }
 
+plot_csc_site_nolabel <- function(background, sand_df, area_txt,
+                            begin=start_date, ending=end_date, 
+                            legend_title="", value_index, value_max){
+  catches <- sand_df %>% filter(dca==area_txt)
+  value.range <- 
+      range(catches[ , value_index])[2] - range(catches[ , value_index])[1]
+  plot.title <- paste0(area_txt, " CSC Sites (", format(begin, "%m/%d"), 
+                       " - ", format(ending, "%m/%d/%Y"), ")")
+  x_range <- diff(ggplot_build(background)[[2]]$ranges[[1]]$x.range)
+  y_range <- diff(ggplot_build(background)[[2]]$ranges[[1]]$y.range)
+  catches[ , value_index] <- 
+      sapply(catches[ , value_index], 
+             function(x) ifelse(x>value_max, value_max, x))
+  p1 <- background +
+    geom_point(data=catches, size=4,  
+               mapping=aes_string(x='x', y='y', 
+                                  color=names(sand_df)[value_index])) +
+    scale_color_gradientn(name=legend_title,  
+                          colors=c("green", "yellow", "red"), 
+                          limits=c(-0.001, value_max+0.001), 
+                          breaks=c(0, value_max/2, value_max), 
+                          labels=c("0", as.character(value_max/2), 
+                                   paste0(">", value_max))) +
+    coord_fixed() +
+    ggtitle(plot.title) +
+    theme(axis.ticks.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.title.x=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.title.y=element_blank(),
+          plot.title=element_text(size=12))
+  p1
+}  
 
-#' Get coordinate ranges for square plot around DCM areas.
-#' 
-#' @param polys. Data frame. Points defining polygons of interest.
-#' @param external_points Data frame. *x* and *y* coordinates of points external 
-#' to polygons to be included in plot range. 
-#' @return A list with the x and y ranges for a sqaure plot around the areas of 
-#' interest.
-get_plot_range <- function(polys, external_points=NULL){
-  p.temp <- ggplot(polys, aes(x=x, y=y)) + geom_path() +
-    geom_point(data=external_points)
-  info <- ggplot_build(p.temp)
-  plot_xrange <- info[[2]]$ranges[[1]]$x.range
-  plot_yrange <- info[[2]]$ranges[[1]]$y.range
-  maxspan <- max(c(plot_xrange[2] - plot_xrange[1], 
-                   plot_yrange[2] - plot_yrange[1]))
-  midpoint <- c(mean(plot_xrange), mean(plot_yrange))
-  xrange <- c(midpoint[1] - (maxspan/2), 
-              midpoint[1] + (maxspan/2))
-  yrange <- c(midpoint[2] - (maxspan/2), 
-              midpoint[2] + (maxspan/2))
-  plot.range <- list(x=xrange, y=yrange)
-  plot.range
-}
 
