378ebf3502f115894300ff311e7e862a7a0800d9
John Bannister <jbannister@airsci.com>
2017-11-16 12:22:17 -0800

    added section to display lidar images

--- R/twb2_functions.R
+++ R/twb2_functions.R
@@ -2,8 +2,10 @@ group_twb2_areas <- function(df1){
     group_df <- data.frame(id2=c("T12-1", "T16", "T3SW", "T3SE", "T2-2", 
                                  "T2-3", "T2-4", "T3NE", "T24 Addition"), 
                            group=c(rep("Central", 2), rep("South", 6), "East"))
-    df1 <- left_join(group_df, df1, by="id2") %>% 
-        filter(bacm_type=="Tillage With BACM Backup")
+    df1 <- left_join(group_df, df1, by="id2") 
+    if ("bacm_type" %in% colnames(df1)){
+        df1 <- df1 %>% filter(bacm_type=="Tillage With BACM Backup")
+    }
     df1$id3 <- df1$group
     df1
 }
---
+++ scripts/twb2_lidar.R
@@ -0,0 +1,41 @@
+cross_walk <- c("T3SW"="T3SW", "T3SE"="T3SE", "T3NE"="T3NE", 
+                "T2-2"="T2-2", "T2-3"="T2-3", "T2-4"="T2-4", 
+                "T16"="T16", "T12"="T12-1", "T24ADD"="T24 Addition")
+gdrive_images <- system(paste0(getwd(), "/gdrive list ", 
+                               "-q \"trashed = false and ", 
+                               "'0B8qHESXOhs-DQTlXa3FKWlpSOHM' in parents and ",
+                               "mimeType = 'image/png'\" ", 
+                               "-m 10000"), intern=T)  
+cmnt_fl <- tempfile()
+write.table(gdrive_images, file=cmnt_fl, quote=F, row.names=F, col.names=F)
+images_list <- read.table(file=cmnt_fl, sep="", header=F, na.strings="", 
+                          skip=1)
+images_list$len <- nchar(images_list$V2)
+images_list$index_date <- 
+    as.Date(paste0('20', 
+                   substr(images_list$V2, 
+                          images_list$len-13, images_list$len-12), "-", 
+                   substr(images_list$V2, 
+                          images_list$len-11, images_list$len-10), "-", 
+                   "01"))
+images_list$id2 <- 
+    cross_walk[substr(images_list$V2, 1, images_list$len-14)]
+recent_images <- images_list %>% group_by(id2) %>%
+    mutate(delta=end_date - index_date) %>%
+    filter(delta==min(delta))
+recent_images <- group_twb2_areas(recent_images)
+lidar_files <- vector(mode='list', length=length(unique(recent_images$id3)))
+names(lidar_files) <- unique(recent_images$id3)
+for (i in names(lidar_files)){
+    lidar_files[[i]] <- 
+        vector(mode='character', length=nrow(filter(recent_images, id3==i)))
+    names(lidar_files[[i]]) <- filter(recent_images, id3==i)$id2
+    for (j in names(lidar_files[[i]])){
+        lidar_files[[i]][j] <- 
+            paste0(tempdir(), "/", filter(recent_images, id2==j)$V2)
+        system(paste0(getwd(), "/gdrive download --force --path ", tempdir(), 
+                      " ", filter(recent_images, id2==j)$V1))
+        system(paste0("convert ", lidar_files[[i]][j], " -resize 5000x400 ",
+        lidar_files[[i]][j]))
+    }
+}
